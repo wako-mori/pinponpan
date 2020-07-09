df222d797693965a5f649232fd07b615e55aff72
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-07-09 19:45:05 +0900

    タイピングの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,19 @@
 class PostsController < ApplicationController
+  include PostsHelper
 
   def index
   end
 
   def new
+    @quest = Post.new
   end
-  
+
+  def create
+    @quest = Post.new(post_params)
+    if @quest.valid?
+      pquest.save
+    else
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, only: [:index, :new]
+
+  resources :games, only: [:index]
 end
