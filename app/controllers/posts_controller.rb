e3b5dc42f4d0b4bb8ea8e8b54bf31ef3b0040665
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-07-13 11:36:01 +0900

    タイピング追加_2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,17 @@ class PostsController < ApplicationController
   def create
     @quest = Post.new(post_params)
     if @quest.valid?
-      pquest.save
+      @quest.save
+      redirect_to root_path, notice: 'タイピングワードが追加されました'
     else
-      redirect_to root_path
+      flash.now[:alert] = 'ワードを入力してください'
+      render :new
     end
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:type_quest)
+  end
+
 end
